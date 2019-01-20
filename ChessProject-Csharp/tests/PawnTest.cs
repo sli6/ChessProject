cfcf9a1b613dabb75a463dc7cc0c9f2da05f08e8
DESKTOP-R60HG2N\shaoy <shaoyun.li@outplay.com>
2019-01-20 00:52:11 +0000

    [update] add unit tests for Pawn.Move before implementing the function

--- ChessProject-Csharp/tests/PawnTest.cs
+++ ChessProject-Csharp/tests/PawnTest.cs
@@ -1,66 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;  
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace SolarWinds.MSP.Chess
 {
     [TestClass]
 	public class PawnTest
 	{
-		private ChessBoard chessBoard;
 		private Pawn pawn;
 
 		[TestInitialize]
 		public void SetUp()
 		{
-			chessBoard = new ChessBoard();
 			pawn = new Pawn(PieceColor.Black);
-		}
-
-        /*
-		[TestMethod]
-		public void ChessBoard_Add_Sets_XCoordinate()
-		{
-			chessBoard.Add(pawn, 6, 3, PieceColor.Black);
-			Assert.AreEqual(pawn.Coordinate.XCoordinate, 6);
-		}
-
-		[TestMethod]
-		public void ChessBoard_Add_Sets_YCoordinate()
-		{
-			chessBoard.Add(pawn, 6, 3, PieceColor.Black);
-			Assert.AreEqual(pawn.Coordinate.YCoordinate, 3);
 		}
 
 		[TestMethod]
 		public void Pawn_Move_IllegalCoordinates_Right_DoesNotMove()
 		{
-			chessBoard.Add(pawn, 6, 3, PieceColor.Black);
-			pawn.Move(MovementType.Move, 7, 3);
-            Assert.AreEqual(pawn.Coordinate.XCoordinate, 6);
-            Assert.AreEqual(pawn.Coordinate.YCoordinate, 3);
 		}
 
 		[TestMethod]
 		public void Pawn_Move_IllegalCoordinates_Left_DoesNotMove()
 		{
-			chessBoard.Add(pawn, 6, 3, PieceColor.Black);
-			pawn.Move(MovementType.Move, 4, 3);
-            Assert.AreEqual(pawn.Coordinate.XCoordinate, 6);
-            Assert.AreEqual(pawn.Coordinate.YCoordinate, 3);
-		}
+        }
 
-		[TestMethod]
+        [TestMethod]
+        public void Pawn_Move_IllegalCoordinates_Backward_DoesNotMove()
+        {
+        }
+
+        [TestMethod]
+        public void Pawn_Move_ILLegalCoordinates_Position_Occupied_DoesNotMove()
+        {
+        }
+
+        [TestMethod]
 		public void Pawn_Move_LegalCoordinates_Forward_UpdatesCoordinates()
 		{
-			chessBoard.Add(pawn, 6, 3, PieceColor.Black);
-			pawn.Move(MovementType.Move, 6, 2);
-			Assert.AreEqual(pawn.Coordinate.XCoordinate, 6);
-            Assert.AreEqual(pawn.Coordinate.YCoordinate, 2);
-		}
-        */
+        }
 
-	}
+        [TestMethod]
+        public void Pawn_Move_LegalCoordinates_Right_Diagonal_UpdatesCoordinates()
+        {
+        }
+
+        [TestMethod]
+        public void Pawn_Move_LegalCoordinates_Left_Diagonal_UpdatesCoordinates()
+        {
+        }
+    }
 }
