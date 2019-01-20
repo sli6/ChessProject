5e77b439e89c6ea528d3a40f809dc4f6ddedc6af
DESKTOP-R60HG2N\shaoy <shaoyun.li@outplay.com>
2019-01-20 00:00:38 +0000

    [add] Add Piece Factory and unit tests

---
+++ ChessProject-Csharp/src/Factories/PieceFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SolarWinds.MSP.Chess
+{
+    public class PieceFactory
+    {
+        public static IPiece Create(PieceType pieceType, PieceColor pieceColor)
+        {
+            switch (pieceType)
+            {
+                case PieceType.Pawn:
+                    return new Pawn(pieceColor);
+                case PieceType.Queen:
+                case PieceType.King:
+                case PieceType.Bishop:
+                case PieceType.Knight:
+                case PieceType.Rook:
+                    throw new NotImplementedException(string.Format("{0} piece has not been implemented", pieceType));
+                default:
+                    throw new InvalidPieceType(string.Format("{0} is not a valid piece type", pieceType));
+            }
+        }
+    }
+}
---
+++ ChessProject-Csharp/tests/PieceFactoryTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SolarWinds.MSP.Chess
+{
+    [TestClass]
+    public class PieceFactoryTest
+    {
+        [TestInitialize]
+        public void SetUp() { }
+
+        [TestMethod]
+        public void Create_Pawn_Piece()
+        {
+            var piece = PieceFactory.Create(PieceType.Pawn, PieceColor.Black);
+            Assert.AreEqual("SolarWinds.MSP.Chess.Pawn", piece.GetType().FullName);
+        }
+
+        [TestMethod]
+        public void Create_Other_Piece_Not_implemented()
+        {
+            var pieceTypes = new PieceType[] {
+                PieceType.Bishop, PieceType.King, PieceType.Queen,
+                PieceType.Knight, PieceType.Rook };
+
+            foreach(PieceType pieceType in pieceTypes)
+            {
+                try
+                {
+                    var piece = PieceFactory.Create(pieceType, PieceColor.Black);
+                }
+                catch(NotImplementedException e)
+                {
+                    Assert.AreEqual(string.Format("{0} piece has not been implemented", pieceType), e.Message);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Create_Invalid_Piece_Type()
+        {
+            var pieceType = (PieceType)7;
+
+            try
+            {
+                var piece = PieceFactory.Create(pieceType, PieceColor.Black);
+            }
+            catch (InvalidPieceType e)
+            {
+                Assert.AreEqual("7 is not a valid piece type", e.Message);
+            }
+        }
+    }
+}
